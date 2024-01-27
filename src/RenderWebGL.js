39841606cc77b441e5c9ce0bc57d862a76f5718e
Nightre <nightscratchs@gmail.com>
2024-01-27 17:38:21 +0800

    Update RenderWebGL.js

--- src/RenderWebGL.js
+++ src/RenderWebGL.js
@@ -2058,6 +2058,9 @@ class RenderWebGL extends EventEmitter {
         const gl = this._gl;
         let currentShader = null;
 
+        const halfNativeSizeX = this._nativeSize[0] / 2
+        const halfNativeSizeY = this._nativeSize[1] / 2
+
         const framebufferSpaceScaleDiffers = (
             'framebufferWidth' in opts && 'framebufferHeight' in opts &&
             opts.framebufferWidth !== this._nativeSize[0] && opts.framebufferHeight !== this._nativeSize[1]
@@ -2071,7 +2074,49 @@ class RenderWebGL extends EventEmitter {
             if (opts.filter && !opts.filter(drawableID)) continue;
 
             const drawable = this._allDrawables[drawableID];
-            /** @todo check if drawable is inside the viewport before anything else */
+
+            const uniforms = {};
+            if (drawMode === 'default' && drawable.skin) {
+                // If rotationCenterDirty or skinScaleDirty is dirty, then set _calculateTransform first
+                // because _rotationAdjusted and _skinScale  needs to call _calculateTransform before using
+                let uniformHasBeenSet = false
+                if (drawable._rotationCenterDirty || drawable._skinScaleDirty) {
+                    Object.assign(uniforms, drawable.getUniforms())
+                    uniformHasBeenSet = true
+                }
+
+                const drawablePosition = drawable._position
+                // position of this texture
+                const positionX = ~~(drawablePosition[0] + 0.5 - drawable._rotationAdjusted[0])
+                const positionY = ~~(drawablePosition[1] + 0.5 - drawable._rotationAdjusted[1])
+                // Half the size
+                const halfSizeX = ~~(drawable._skinScale[0] / 2 + 0.5)
+                const halfSizeY = ~~(drawable._skinScale[1] / 2 + 0.5)
+
+                // The leftTop and rightBottomX of the sprite must be enlarged, otherwise there will be problems when rotating
+                const maxHalfSize = Math.max(halfSizeX, halfSizeY)
+
+                const leftTopX = positionX - halfSizeX - maxHalfSize
+                // Y-axis is reversed
+                const leftTopY = positionY + halfSizeY + maxHalfSize 
+
+                const rightBottomX = positionX + halfSizeX + maxHalfSize
+                // Y-axis is reversed
+                const rightBottomY = positionY - halfSizeY - maxHalfSize 
+
+                if (rightBottomX < -halfNativeSizeX || rightBottomY > halfNativeSizeY) {
+                    continue
+                }
+                if (leftTopX > halfNativeSizeX || leftTopY < -halfNativeSizeY) {
+                    continue
+                }
+                if (!uniformHasBeenSet) {
+                    // If unconfirm was not set before
+                    Object.assign(uniforms, drawable.getUniforms())
+                }
+            } else {
+                Object.assign(uniforms, drawable.getUniforms())
+            }
 
             // Hidden drawables (e.g., by a "hide" block) are not drawn unless
             // the ignoreVisibility flag is used (e.g. for stamping or touchingColor).
@@ -2091,7 +2136,6 @@ class RenderWebGL extends EventEmitter {
             // Skip private skins, if requested.
             if (opts.skipPrivateSkins && drawable.skin.private) continue;
 
-            const uniforms = {};
 
             let effectBits = drawable.enabledEffects;
             effectBits &= Object.prototype.hasOwnProperty.call(opts, 'effectMask') ? opts.effectMask : effectBits;
@@ -2112,8 +2156,7 @@ class RenderWebGL extends EventEmitter {
             }
 
             Object.assign(uniforms,
-                drawable.skin.getUniforms(drawableScale),
-                drawable.getUniforms());
+                drawable.skin.getUniforms(drawableScale));
 
             // Apply extra uniforms after the Drawable's, to allow overwriting.
             if (opts.extraUniforms) {
